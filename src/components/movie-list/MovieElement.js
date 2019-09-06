8a6a27b403eacd562056d81f099416aa279052a5
Daouda Faye NGOM <dfngom@gmail.com>
2019-09-06 11:47:45 -0400

    React> Chapitre 6. Lecon 2: Refactorisation de l'application

--- src/components/movie-list/MovieElement.js
+++ src/components/movie-list/MovieElement.js
@@ -4,13 +4,13 @@ export default class MovieElement extends Component {
 
     render() {
         return (
-            <div className="w-25 p-2">
-                <div className="card">
-                    <img alt="film" src="https://www.creative-ones.com/assets/blog/2016/09/aEsti-pregatit-pentru-viitor-Internet-of-Things-IoT-este-tot-mai-aproape-de-tine.jpg" className="card-img-top" />
-                    <div className="card-body">
-                        <h5 className="card-title">CREATIVE ONES</h5>
-                        <p className="card-text">These days it plays on the screen I.T. with Pierce Brosnan in the lead role. Like any American film, there are a number of reviews that catalog it as a poor film, but there are also some good reviews.
-We live in the smart era of technology and custom applications. We have smartphone, smart TV, smartwatch. We want everything around us to be smarter and give us more comfort.</p>
+            <div className="w-50 p-2">
+                <div className="border d-flex">
+                    <img width="150" height="200" alt="film" src="https://www.creative-ones.com/assets/blog/2016/09/aEsti-pregatit-pentru-viitor-Internet-of-Things-IoT-este-tot-mai-aproape-de-tine.jpg" />
+                    <div className="flex-fill d-flex flex-column p-3">
+                        <h5 >Titre</h5>
+                        <hr className="w-100" />
+                        <p >These days it plays on the screen I.T. with Pierce Brosnan in the lead role. Like any American film, there are a number of reviews that catalog it as a poor film....</p>
                     </div>
                 </div>
             </div>
