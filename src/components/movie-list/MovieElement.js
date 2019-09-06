f09418ad670a94a819333342f62a35c206566474
Daouda Faye NGOM <dfngom@gmail.com>
2019-09-06 13:47:25 -0400

    React> Chapitre 6. Lecon 4: Mise en place de la selection d'un film

--- src/components/movie-list/MovieElement.js
+++ src/components/movie-list/MovieElement.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react'
 
 export default class MovieElement extends Component {
 
+    mouseEnter = () => {
+        this.props.updateSelectedMovie(this.props.movie.title);
+    }
+
     render() {
         return (
-            <div className="w-50 p-2">
+            <div onMouseEnter={this.mouseEnter} className="w-50 p-2">
                 <div className="border d-flex">
-                    <img width="150" height="200" alt="film" src="https://www.creative-ones.com/assets/blog/2016/09/aEsti-pregatit-pentru-viitor-Internet-of-Things-IoT-este-tot-mai-aproape-de-tine.jpg" />
+                    <img width="150" height="200" alt="film" src={this.props.movie.img} />
                     <div className="flex-fill d-flex flex-column p-3">
-                        <h5 >Titre</h5>
+                        <h5 >{this.props.movie.title}</h5>
                         <hr className="w-100" />
-                        <p >These days it plays on the screen I.T. with Pierce Brosnan in the lead role. Like any American film, there are a number of reviews that catalog it as a poor film....</p>
+                        <span>{this.props.movie.details}</span>
                     </div>
                 </div>
             </div>
