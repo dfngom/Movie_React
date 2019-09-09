1c0e7cfb8030bd4071f840fd7e12ff01cb5696b3
Daouda Faye NGOM <dfngom@gmail.com>
2019-09-09 07:09:44 -0400

    React> Chapitre 9. Lecon: Utilisation du rendu conditionnel et loader

---
+++ src/components/utils/Loading.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div style={{ minHeight: '100vh' }} className="d-flex flex-row justify-content-center align-items-center">
+            <img alt="loading gif" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpitWwdgAuhii-wIDsdLXH4uWjqx0uXo5Nzv7GmUDutP40jnKRJA" />
+        </div>
+    );
+}
