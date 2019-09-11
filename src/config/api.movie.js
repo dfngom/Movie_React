9fd05ff0cea5ad031ad2f4b4bed938505ffcf1fe
Daouda Faye NGOM <dfngom@gmail.com>
2019-09-11 11:02:38 -0400

    React>. Chapitre 13. Lecon 2: Utilisation d'une API Externe

--- src/config/api.movie.js
+++ src/config/api.movie.js
@@ -8,6 +8,7 @@ const apiMovie = axios.create({
 apiMovie.interceptors.request.use(req => {
     req.headers['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYTA1ZGI1YzUwOTI4ZmE5OTU2YzkzOGUwZmFlODEyNSIsInN1YiI6IjVkNzdmNGRkNmJkZWMzMDAxMDU0MzBkYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.l3B1TffQB_zOTHwKZQdmzusPyKUDBBA48IIPeYLjXVc';
 
-    return req
+    return req;
 })
+
 export default apiMovie;
